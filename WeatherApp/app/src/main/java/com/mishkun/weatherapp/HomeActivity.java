b82f7a147438ef3195dd2edcbc2fcd47cae00405
mishkun.lev <mishkun.lev@gmail.com>
2017-07-20 13:58:41 +0300

    Added canonical splash screen

--- WeatherApp/app/src/main/java/com/mishkun/weatherapp/HomeActivity.java
+++ WeatherApp/app/src/main/java/com/mishkun/weatherapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.mishkun.weatherapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -32,11 +33,11 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
